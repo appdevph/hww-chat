8c61794d0a20c9c220bc88cf804a385ed2eb82bc
rorenz@gmail.com <rorenz@gmail.com>
2015-04-19 21:07:19 +0800

    chatbox changes

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).foundation();
+            $('#discussion').fadeOut();
+            $('#loginForm').fadeIn();
+// CHAT BOX
+$(document).ready(function(){
+    $('#chat').click(function(){
+		var hidden = $('.hidden');
+		if (hidden.hasClass('visible')){
+			hidden.animate({"left":"-300px"}, "slow").removeClass('visible');
+		} else {
+			hidden.animate({"left":"0px"}, "slow").addClass('visible');
+		}
+    });
+});
+
+$(document).ready(function() {
+    $('#close').click(function(){
+    var hidden = $('.hidden');
+    if (hidden.hasClass('visible')){
+        hidden.animate({"left":"-300px"}, "slow").removeClass('visible');
+    } else {
+        hidden.animate({"left":"0px"}, "slow").addClass('visible');
+    }
+    });	
+});
+// END CHATBOX
